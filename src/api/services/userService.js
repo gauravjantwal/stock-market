046b93081381d8e02c0be881ae5155867a7b94e3
Gaurav Singh Jantwal <gaurav.jantwal@gmail.com>
2024-02-12 20:42:29 +0530

    Fixed user registration

--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -1,16 +1,14 @@
-const sqlString = require('sqlstring');
+//const sqlString = require('sqlstring');
 const { BadRequestError } = require('../models/errors');
 const db = require('../utils/db');
 const User = db.User;
 
 const signUp = async (req, res) => {
-    const userEmail = sqlString.escape(req.body.email);
+    //const userEmail = sqlString.escape(req.body.email);
+    const userEmail = req.body.email;
     let user = await User.findOne({ email: userEmail });
-    console.log("=============");
-    console.log(user, userEmail);
-    console.log("=============");
     if (user) {
-        throw new BadRequestError('User already registered with email ' + userEmail + '.', 409);
+        throw new BadRequestError('User already registered with email "' + userEmail + '".', 409);
     }
     user = new User(req.body);
     await user.save(user);
