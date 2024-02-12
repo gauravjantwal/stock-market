84513b2c2e4497de54d97d2a76f1849a1f878469
Gaurav Singh Jantwal <gaurav.jantwal@gmail.com>
2024-02-12 12:49:22 +0530

    Fixed code scanning alert `js/missing-rate-limiting`

--- src/api/router/router.js
+++ src/api/router/router.js
@@ -2,8 +2,15 @@ const usercontroller = require('../controllers/usercontroller');
 const dashboardcontroller = require('../controllers/dashboardcontroller');
 const timeseriescontroller = require('../controllers/timeseriesdailycontroller');
 
+
+
 module.exports = function (router) {
     router.get('/dashboard', dashboardcontroller.getdashboard);
     router.get('/timeseriesdaily', timeseriescontroller.gettimeseriesdaily);
-    router.post('/user/signup', usercontroller.signup);
+    router.post('/user/signup', RateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 5, // max 100 requests per windowMs
+        headers: true,
+        message: 'You have exceeded your 3 requests per 15 minute limit.'
+      }), usercontroller.signup);
 };
