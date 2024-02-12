afa8d621715a8dd21b7e2d7fe1827642062eb767
Vaibhav <vaibhav.garg@emids.com>
2024-02-12 12:38:39 +0530

    company overview and news & sentiments api

--- src/api/controllers/dashboardcontroller.js
+++ src/api/controllers/dashboardcontroller.js
@@ -1,20 +1,25 @@
-const axios = require("axios");
+
+const axios = require('axios');
 
 // Define the URL of the third-party API
-const apiUrl =
-  "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo";
+const apiUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo';
 
 // Make a GET request to the API
-exports.getdashboard = function (req, res) {
-  axios
-    .get(apiUrl)
-    .then((response) => {
-      // Handle successful response
-      //console.log('Response:', response.data);
-      res.send(response.data);
-    })
-    .catch((error) => {
-      // Handle error
-      console.error("Error:", error);
-    });
-};
+exports.getdashboard = function(req,res){
+    axios.get(apiUrl)
+      .then(response => {
+        // Handle successful response
+        //console.log('Response:', response.data);
+        res.send(response.data);
+      })
+      .catch(error => {
+        // Handle error
+        console.error('Error:', error);
+      });
+    };
+
+
+    
+    
+
+
--- src/api/router/router.js
+++ src/api/router/router.js
@@ -1,9 +1,19 @@
-const usercontroller=require('../controllers/usercontroller');
-const dashboardcontroller=require('../controllers/dashboardcontroller');
-const timeseriescontroller=require('../controllers/timeseriesdailycontroller');
+const usercontroller = require("../controllers/usercontroller");
+const dashboardcontroller = require("../controllers/dashboardcontroller");
+const timeseriescontroller = require("../controllers/timeseriesdailycontroller");
+const newsandsentimentscontroller = require("../controllers/newsandsentimentscontroller");
+const companyoverviewcontroller = require("../controllers/companyoverviewcontroller");
 
-module.exports=function(router){
-    router.get('/user',usercontroller.getdefault);
-    router.get('/dashboard',dashboardcontroller.getdashboard); 
-    router.get('/timeseriesdaily',timeseriescontroller.gettimeseriesdaily);     
+module.exports = function (router) {
+  router.get("/user", usercontroller.getdefault);
+  router.get("/dashboard", dashboardcontroller.getdashboard);
+  router.get("/timeseriesdaily", timeseriescontroller.gettimeseriesdaily);
+  router.get(
+    "/newsandsentiments",
+    newsandsentimentscontroller.getnewsandsentiments
+  );
+  router.get(
+    "/companyoverview/:stocksymbol",
+    companyoverviewcontroller.getcompanyoverview
+  );
 };
