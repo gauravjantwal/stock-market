8aeec5a757253c034da17a27d1bad12a824961f1
Vaibhav <vaibhav.garg@emids.com>
2024-02-12 12:23:34 +0530

    test commit

--- src/api/controllers/dashboardcontroller.js
+++ src/api/controllers/dashboardcontroller.js
@@ -1,25 +1,20 @@
-
-const axios = require('axios');
+const axios = require("axios");
 
 // Define the URL of the third-party API
-const apiUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo';
+const apiUrl =
+  "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo";
 
-// Make a GET request to the API
-exports.getdashboard = function(req,res){
-    axios.get(apiUrl)
-      .then(response => {
-        // Handle successful response
-        //console.log('Response:', response.data);
-        res.send(response.data);
-      })
-      .catch(error => {
-        // Handle error
-        console.error('Error:', error);
-      });
-    };
-
-
-    
-    
-
-
+// Make a GET request to the API ABC
+exports.getdashboard = function (req, res) {
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      // Handle successful response
+      //console.log('Response:', response.data);
+      res.send(response.data);
+    })
+    .catch((error) => {
+      // Handle error
+      console.error("Error:", error);
+    });
+};
