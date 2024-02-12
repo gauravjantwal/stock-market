c9575a2f4c362d080b31927897d8f8712cdd78bb
Gaurav Singh Jantwal <gaurav.jantwal@gmail.com>
2024-02-12 12:41:02 +0530

    Fixed code scanning alerts `js/sql-injection`

--- src/api/controllers/usercontroller.js
+++ src/api/controllers/usercontroller.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 
 exports.signup = async (req, res) => {
-    let user = await User.findOne({ email: req.body.email });
+    let user = await User.findOne({ email:  SqlString.escape(req.body.email) });
     if (user) {
         throw new Error("User already registered with requested email.");
     }
