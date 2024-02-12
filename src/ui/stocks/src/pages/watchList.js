50cb417e98fbcfec4b3dde8b6b9b02b187675f1c
Lavanya_Nagarajan <lavanya.nagarajan@cytel.com>
2024-02-12 11:36:49 +0530

    watchlist-details

--- src/ui/stocks/src/pages/watchList.js
+++ src/ui/stocks/src/pages/watchList.js
@@ -1,45 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react';
+import WatchListDetails from './watchlist-details';
 
 const WatchList = () => {
-    return(
-        <div>
-            <h3>All Watchlists</h3>  
-            <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-            <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Company</th>
-      <th scope="col">Market Price</th>
-      <th scope="col">Day Change</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Maruti Suzuki</td>
-      <td>1500</td>
-      <td>-137</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Tata Motors</td>
-      <td>2000</td>
-      <td>-345</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Volkswagan</td>
-      <td>3000</td>
-      <td>400</td>
-    </tr>
-  </tbody>
-</table>
-</div>
-    );
-}
+  // Define some sample table data
+  const tableData = [
+    { id: 1, companyName: 'TataMotors', marketPrice: 3000, dayChange : -345 },
+    { id: 2, companyName: 'Maruti Suzuki', marketPrice: 2000, dayChange : -347 },
+    { id: 3, companyName: 'Volkswagan', marketPrice: 1000, dayChange : -678 },
+  ];
+
+  // State to manage whether to show the second table or not
+  const [showTable, setShowTable] = useState(false);
+  // State to hold the data of the clicked row
+  const [clickedRowData, setClickedRowData] = useState(null);
+
+  // Event handler to handle row click
+  const handleRowClick = (rowData) => {
+    setShowTable(true);
+    setClickedRowData(rowData);
+  };
+
+  return (
+    <div>
+        <div className="row">
+        <div className="col-6 col-md-6 float-left">
+        <form className="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>CompanyName <i class="fa-solid fa-caret-down"></i></th>
+              <th>MarketPrice</th>
+              <th>DayChange</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.map((row) => (
+              <tr key={row.id} onClick={() => handleRowClick(row)}>
+                <td>{row.id}</td>
+                <td>{row.companyName}</td>
+                <td>{row.marketPrice}</td>
+                <td>{row.dayChange}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        </div>
+        <div className="col-6 col-md-6 float-left">{showTable && <WatchListDetails rowData={clickedRowData} />}
+        </div>
+    </div>
+    </div>
+  );
+};
 
 export default WatchList;
