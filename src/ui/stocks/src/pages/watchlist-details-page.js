31d981af93d2d58dcb4483614946ef9cb5eb2023
Lavanya_Nagarajan <lavanya.nagarajan@cytel.com>
2024-02-13 13:57:35 +0530

    watchlist-details

---
+++ src/ui/stocks/src/pages/watchlist-details-page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useParams, useLocation } from 'react-router-dom';
+import tata from "./Images/tata-logo.png";
+
+
+const WatchListDetailsPage = () => {
+    const {name} = useParams();
+    let location = useLocation();
+      console.log(location);
+    return (
+        <div>
+        <h3>Watch list details</h3>
+        <p>{name}</p>
+        </div>
+    );
+};
+export default WatchListDetailsPage;
