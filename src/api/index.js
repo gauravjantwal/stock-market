0f286437e6049501b0955e3216f2dbddf0b4dfa0
Nihar Shaik <shaikn@emids.com>
2024-02-15 16:15:43 +0530

    SignOut changes

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,8 @@ const appRoutes = require('./router/index');
 const config = require("./config/config.json");
 const app = express();
 const port = process.env.port || config.port;
+const session = require('express-session');
+const monogoDBSession = require('connect-mongodb-session')(session);
 
 
 const bodyparse = require("body-parser")
@@ -16,6 +18,28 @@ require("dotenv").config();
 app.use(bodyparse.json())
 app.use(cookieparser())
 app.use(cors())
+
+//Connecting to MongoDB to store session
+const store = new monogoDBSession({
+    uri: config.connectionString,
+    collection: 'Users',
+});
+
+//store session in database
+app.use(session({
+    secret: 'botnyuserdetails',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 365 * 24 * 60 * 60 * 1000 },
+    store: store   
+    }));
+
+  app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');        
+    }
+    next();
+});
 
 const router = express.Router();
 appRoutes(router); // Register all application routes
