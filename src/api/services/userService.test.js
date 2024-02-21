53e8e1ac3fa9f87eed2b37b45547ee6f2dcda6bd
Nihar Shaik <shaikn@emids.com>
2024-02-21 11:46:46 +0530

    Testcases for SignUp & SignIn

---
+++ src/api/services/userService.test.js
@@ -0,0 +1,60 @@
+const { postUserSignUp,
+    postUserSignIn,
+    getUserSignOut
+} = require("../controllers/usercontroller");
+
+jest.mock('../controllers/usercontroller');
+
+//SignUp Testcase
+describe("UserSignUp", () => {
+    it("should SignUp the user", async () => {
+  
+      // Mocking request and response objects
+      const req = { params: { email: "test@abc.com" },
+                    params: {name :"test"},
+                    params: {password:"test@123"}
+    };
+     
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        send: jest.fn(),
+      };
+  
+      // Calling the function
+      await postUserSignUp(req, res);
+  
+      // Assertions
+      expect(postUserSignUp).toHaveBeenCalled();
+  
+    });
+  
+  });
+  
+//SignIn Testcase
+describe("UserSignIn", () => {
+    it("should SignIn the user", async () => {
+  
+      // Mocking request and response objects
+      const req = { params: { email: "test@abc.com" },
+                    params: {password:"test@123"}
+    };
+     
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        send: jest.fn(),
+      };
+  
+      // Calling the function
+      await postUserSignIn(req, res);
+  
+      // Assertions
+      expect(postUserSignIn).toHaveBeenCalled();
+  
+    });
+  
+  });
+  
+
+
